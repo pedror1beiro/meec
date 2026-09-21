e911e8d9636c23b7bf086f08dcebdbe4a50606a8
Pedro Ribeiro <167824758+pedror1beiro@users.noreply.github.com>
2026-09-21 18:13:30 +0000

    style: format ROS1 C++ sources

--- unidades-curriculares/rsdis/exercicio-1/ros1/rsdis_t1a/src/listener.cpp
+++ unidades-curriculares/rsdis/exercicio-1/ros1/rsdis_t1a/src/listener.cpp
@@ -1,14 +1,12 @@
 #include "ros/ros.h"
 #include "std_msgs/Float32.h"
 
-void floatCallback(const std_msgs::Float32::ConstPtr& msg) {
-  ROS_INFO("I heard: [%f]", msg->data);
-}
+void floatCallback(const std_msgs::Float32::ConstPtr& msg) { ROS_INFO("I heard: [%f]", msg->data); }
 
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "listener");
-  ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("float_topic", 1000, floatCallback);
-  ros::spin();
-  return 0;
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "listener");
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe("float_topic", 1000, floatCallback);
+    ros::spin();
+    return 0;
 }
--- unidades-curriculares/rsdis/exercicio-1/ros1/rsdis_t1a/src/talker.cpp
+++ unidades-curriculares/rsdis/exercicio-1/ros1/rsdis_t1a/src/talker.cpp
@@ -2,18 +2,18 @@
 #include "std_msgs/Float32.h"
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "talker");
-  ros::NodeHandle nh;
-  ros::Publisher floatPublisher = nh.advertise<std_msgs::Float32>("float_topic", 1000);
-  ros::Rate loopRate(50); // 50 Hz
+    ros::init(argc, argv, "talker");
+    ros::NodeHandle nh;
+    ros::Publisher floatPublisher = nh.advertise<std_msgs::Float32>("float_topic", 1000);
+    ros::Rate loopRate(50);  // 50 Hz
 
-  while (ros::ok()) {
-    std_msgs::Float32 msg;
-    msg.data = 3.14; // Valor de exemplo
-    ROS_INFO("Publishing: %f", msg.data);
-    floatPublisher.publish(msg);
-    ros::spinOnce();
-    loopRate.sleep();
-  }
-  return 0;
+    while (ros::ok()) {
+        std_msgs::Float32 msg;
+        msg.data = 3.14;  // Valor de exemplo
+        ROS_INFO("Publishing: %f", msg.data);
+        floatPublisher.publish(msg);
+        ros::spinOnce();
+        loopRate.sleep();
+    }
+    return 0;
 }
